18d1ac66e391e7b78a3aeaeaaf0679750c5fff3c
AlejandroOrozco1205 <146884559+AlejandroOrozco1205@users.noreply.github.com>
2025-12-08 20:31:19 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
 console.log("Cargando servidor...");
-
+ 
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./src/config/db');
-
+ 
 const app = express();
-
+ 
+connectDB(process.env.MONGO_URI);
+ 
 app.use(cors());
 app.use(express.json());
-
-// Test route
-app.get('/test', (req, res) => {
-    res.json({ msg: 'Servidor funcionando!' });
-});
-
-// Conectar a Mongo
-connectDB(process.env.MONGO_URI);
-
-// Rutas
+ 
 console.log("Cargando rutas...");
+ 
 app.use('/api/auth', require('./src/routes/auth'));
 console.log("Ruta /api/auth registrada correctamente");
+ 
 app.use('/api/notas', require('./src/routes/notas'));
 app.use('/api/usuarios', require('./src/routes/usuarios'));
-
-// 404
-app.use((req, res) => res.status(404).json({ msg: 'Endpoint not found' }));
-
+ 
+const frontendPath = path.join(__dirname, 'frontend');
+ 
+app.use(express.static(frontendPath));
+ 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(frontendPath, 'home.html'));
+});
+ 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
     console.log(`Servidor corriendo en http://localhost:${PORT}`)
